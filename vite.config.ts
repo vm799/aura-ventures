57fda226251bb364a3b33b58a750f329ac650507
Vaishali <vaishalimehmi@yahoo.co.uk>
2025-08-26 20:19:27 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/aura-ventures/', // Set base for GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
